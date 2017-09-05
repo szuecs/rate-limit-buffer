03cfcfbad427f5af56e2ef87f98caa5637fb44a0
Sandor Szuecs <sandor.szuecs@zalando.de>
2017-09-05 21:48:38 +0200

    fix godoc

--- circularbuffer.go
+++ circularbuffer.go
@@ -44,11 +44,11 @@ func (cb *CircularBuffer) InUse() bool {
 
 // Free returns if there is space or the bucket is full for the current time.
 // Example:
-// time.Now(): 5
-// timeWindow: 2
-// [1 2 3 4]
-//        ^
-// 5-2 = 3 --> 2 free slots [1,2] are too old and are Free already
+//   time.Now(): 5
+//   timeWindow: 2
+//   [1 2 3 4]
+//          ^
+//   5-2 = 3 --> 2 free slots [1,2] are too old and are Free already
 func (cb *CircularBuffer) Free() bool {
 	return cb.slots[cb.offset].Add(cb.timeWindow).Before(time.Now())
 }
@@ -56,16 +56,16 @@ func (cb *CircularBuffer) Free() bool {
 // Add adds an element to the next free bucket in the buffer and
 // returns true. It returns false if there is no free bucket.
 // Example
-// [_ _ _ _]
-//  ^
-// [1 _ _ _]
+//   [_ _ _ _]
 //    ^
-// [1 2 _ _]
+//   [1 _ _ _]
 //      ^
-// [1 2 3 _]
+//   [1 2 _ _]
 //        ^
-// [1 2 3 4]
-//  ^
+//   [1 2 3 _]
+//          ^
+//   [1 2 3 4]
+//    ^
 func (cb *CircularBuffer) Add(t time.Time) bool {
 	if cb.Free() {
 		cb.slots[cb.offset] = t
