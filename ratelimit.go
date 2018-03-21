21ae2c81fd6cf544d07d57850e3662e056ddceef
Hanno Hecker <hanno@zalando.de>
2018-03-21 08:58:21 +0100

    add missing Resize() to interface

--- ratelimit.go
+++ ratelimit.go
@@ -14,6 +14,7 @@ type RateLimiter interface {
 	// Close cleans up the RateLimiter implementation.
 	Close()
 	Delta(string) time.Duration
+	Resize(string, int)
 }
 
 // NewRateLimiter returns a new initialized RateLimitter with maxHits
